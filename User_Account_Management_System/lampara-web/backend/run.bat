fa49cdb31687bca6d2ccf13d74ccc7a0a0c52473
Lucky Bautista <202511141@gordoncollege.edu.ph>
2026-06-26 13:38:49 +0800

    Fix merge conflict in run.bat
    Removed merge conflict markers from run.bat.

--- User_Account_Management_System/lampara-web/backend/run.bat
+++ User_Account_Management_System/lampara-web/backend/run.bat
@@ -1,7 +1,4 @@
 @echo off
-<<<<<<< HEAD:User_Account_Management_System/lampara-web/backend/run.bat
-=======
 set LAMPARA_EMAIL='';
 set LAMPARA_EMAIL_PASSWORD='';
 java -jar target/lampara-backend.jar
->>>>>>> 468eae2702954ad67298784dbb5531a345ff7d7a:backend/run.bat
